b3405f836baa144a4f1d5457c89b1698a03a19c8
mq <luumanhquan.91@gmail.com>
2020-12-14 11:40:08 +0700

    .

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ const sleep = (ms) => {
 const main = async () => {
   await saveDataDB();
   await getCategoriesSheets();
-  sleep(1000);
+  await sleep(1000);
   const shops = await db("shopbase_stores").select("shop");
   const arrayShop = shops.map((item) => item.shop);
   for (let item of arrayShop) {
     await updateToken(item);
-    sleep(100);
+    await sleep(100);
     await insertProduct(item);
   }
-  await sleep(10000);
+  await sleep(100);
   await productBatch();
 };
 
